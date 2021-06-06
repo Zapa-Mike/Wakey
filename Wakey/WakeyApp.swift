54d88fb8e9da6d37b29146d68ec3e97d93d8a77b
Zapa-Mike <zapa.mike@yahoo.com>
2021-06-06 17:13:49 +0200

    Initial Commit

---
+++ Wakey/WakeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeyApp.swift
+//  Wakey
+//
+//  Created by Student on 06.06.21.
+//
+
+import SwiftUI
+
+@main
+struct WakeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
